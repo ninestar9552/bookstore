9bff7eb209e7cf75790daf1f4c8c1beedb02d512
ninestar9552 <ninestra9552@gmail.com>
2024-06-10 20:21:52 +0900

    Initial Commit

---
+++ bookstore/bookstoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookstoreApp.swift
+//  bookstore
+//
+//  Created by 차순혁 on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct bookstoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
